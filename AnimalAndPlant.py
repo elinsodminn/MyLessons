be71bdebc0201cab38cc1c8f16d34a782afec62a
elinsnt <elinsodminn@gmail.com>
2024-08-27 23:53:26 +0500

    Final edit lesson

---
+++ AnimalAndPlant.py
@@ -0,0 +1,59 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        self.alive = True
+        self.fed = False
+
+class Plant:
+    def __init__(self, name):
+        self.name = name
+        self.edible = False
+
+class Mammal(Animal):
+    def eat(self, food):
+        if food.edible:
+            self.fed = True
+            print(f"{self.name} съел {food.name}")
+        else:
+            self.alive = False
+            print(f"{self.name} не стал есть {food.name}")
+
+class Predator(Animal):
+    def eat(self, food):
+        if food.edible:
+            self.fed = True
+            print(f"{self.name} съел {food.name}")
+        else:
+            self.alive = False
+            print(f"{self.name} не стал есть {food.name}")
+
+class Flower(Plant):
+    def __init__(self, name):
+        super().__init__(name)
+
+class Fruit(Plant):
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = True  # Переопределяем фрукт как съедобный
+
+# Создание объектов
+
+flower = Flower("Роза")
+fruit = Fruit("Яблоко")
+
+mammal = Mammal("Кролик")
+predator = Predator("Волк")
+
+# Результат взаимодействия
+
+print(f"{mammal.name}")
+print(f"{predator.name}")
+
+print(f"{mammal.alive}")
+print(f"{predator.fed}")
+
+mammal.eat(flower)
+predator.eat(fruit)
+
+print(f"{mammal.alive}")
+print(f"{predator.fed}")
